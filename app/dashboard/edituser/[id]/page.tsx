c72402a1cf2e5bdbe0134c1ce1f3eb4deaf0afac
Coddy21 <yassermagelna@gmail.com>
2024-12-02 10:15:32 +0800

    uodate

--- app/dashboard/edituser/[id]/page.tsx
+++ app/dashboard/edituser/[id]/page.tsx
@@ -1,8 +1,6 @@
 import EditUserList from "@/components/VIEW/EditUserList";
 import React from "react";
 
-export default function EditUser({ params }: { params:any }) {
-  return <EditUserList params={params.id}  ></EditUserList>;
 export default async function EditUser({ params }: { params: any }) {
   const {id} = await params;
   return <EditUserList params={id}></EditUserList>;
