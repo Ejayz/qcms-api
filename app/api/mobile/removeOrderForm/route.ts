d0d437212fb8f8ee02546368ad45a9f778905b9b
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-12-09 01:17:38 +0800

    Update route.ts

--- app/api/mobile/removeOrderForm/route.ts
+++ app/api/mobile/removeOrderForm/route.ts
@@ -1,6 +1,6 @@
 
 import { createClient } from "@/utils/supabase/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
 
@@ -14,15 +14,9 @@ export async function POST(req: NextRequest) {
     .eq("order_id", order_id);
     
   if (error) {
-    return {
-      status: 500,
-      data: { error: error.message },
-    };
+  return NextResponse.json({code:500, error: error.message }, { status: 500 });
   }
-  return {
-    status: 200,
-    data: data,
-  };
+  return NextResponse.json({ code: 200, data: data }, { status: 200 }); 
 }
 
 
