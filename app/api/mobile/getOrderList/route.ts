10f02dd73c26d366378fde73849e3454e17e343d
Ejayz <32452006+Ejayz@users.noreply.github.com>
2025-01-16 09:58:58 +0800

    Update route.ts

--- app/api/mobile/getOrderList/route.ts
+++ app/api/mobile/getOrderList/route.ts
@@ -2,13 +2,38 @@ import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const search = req.nextUrl.searchParams.get("search");
+  const startDate = req.nextUrl.searchParams.get("startDate");
+  const endDate = req.nextUrl.searchParams.get("endDate");
+
   const supabase = await createClient();
-  const { data, error } = await supabase
+  // let totalDataBasedOnQuery = 0;
+
+  // Base query
+  let query = supabase
     .from("tbl_orders_form")
-    .select("*,tbl_customer(*),tbl_article(*),tbl_assignee_history(*)")
+    .select("* ,tbl_customer(*),tbl_article(*)", { count: "exact" })
     .eq("is_exist", true)
+    .or(`order_fabrication_control.ilike.%${search}%`)
+
+
+  // Add date filters
+  if (startDate && endDate) {
+    const adjustedStartDate = `${startDate}T00:00:00`; // Ensure start of the day
+    const adjustedEndDate = `${endDate}T23:59:59`; // Ensure end of the day
+    query = query
+      .gte("created_at", adjustedStartDate)
+      .lte("created_at", adjustedEndDate);
+  }
+
+  const { data, error, count } = await query;
+  console.log(data, error, count);
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
+  } else {
+    return NextResponse.json(
+      { data, total_count: count || 0 },
+      { status: 200 }
+    );
   }
-  return NextResponse.json({ data: data, error: error }, { status: 200 });
 }
