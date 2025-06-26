95d367bed7a0a850a5f35578c38793e6f962a534
Ejayz <32452006+Ejayz@users.noreply.github.com>
2025-06-27 05:12:38 +0800

    Update route.ts

--- app/api/mobile/getCustomerList/route.ts
+++ app/api/mobile/getCustomerList/route.ts
@@ -2,11 +2,18 @@ import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const search = req.nextUrl.searchParams.get("search");
   const supabase = await createClient();
   const { data, error } = await supabase
     .from("tbl_customer")
     .select("*")
-    .eq("is_exist", true).order("created_at", { ascending: false });
+    .ilikeAnyOf("company_name", [
+      `%${search}%`,
+      `%${search?.toUpperCase()}%`,
+      `%${search?.toLowerCase()}%`,
+    ])
+    .eq("is_exist", true)
+    .order("created_at", { ascending: false });
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
